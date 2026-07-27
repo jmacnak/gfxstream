06acbe9ad4ebbe09d33643d8c52d000c176db077
Jeongik Cha <jeongik@google.com>
2026-07-26 22:21:12 -0700

    Revert "Add DmaBuf external memory mode"
    This reverts commit 0c19b8d24a5a7c24355bda612570e2718572e94f.
    
    Reason for revert: b/539295062
    
    Bug: 451503041
    Bug: 400999642
    Change-Id: I13b04cc120dad28cd7de757f5feb5db55b69d7b0

--- host/vulkan/vk_common_operations.h
+++ host/vulkan/vk_common_operations.h
@@ -145,6 +145,8 @@ class VkEmulation {
 
     bool supportsExternalMemoryImport() const;
 
+    bool supportsDmaBuf() const;
+
     bool supportsExternalMemoryHostProperties() const;
 
     bool isSwapchainEnabled() const;
@@ -482,6 +484,7 @@ class VkEmulation {
         bool hasComputeQueueFamily = false;
         bool supportsExternalMemoryImport = false;
         bool supportsExternalMemoryExport = false;
+        bool supportsDmaBuf = false;
         bool supportsDriverProperties = false;
         bool supportsExternalMemoryHostProps = false;
         bool supportsSwapchain = false;
